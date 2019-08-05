b1a16cbbb91da2b1bfa42b56416e70e841e79ab8
Lucas Garron <code@garron.net>
2019-08-05 18:00:02 -0400

    Initial working version.

---
+++ index.ts
@@ -0,0 +1,42 @@
+import {get, create} from "@github/webauthn-json"
+
+window.addEventListener("load", function() {
+  const createButton = document.createElement("button");
+  createButton.textContent = "create"
+  createButton.addEventListener("click", function() {
+    create({
+      publicKey: {
+        challenge: "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=",
+        rp: {
+          name: "Localhost, Inc."
+        },
+        user: {
+          id: "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII=",
+          name: "Hi there",
+          displayName: "Test User",
+        },
+        pubKeyCredParams: [{
+          type: "public-key",
+          alg: -7
+        }]
+      }
+    });
+  })
+  document.body.appendChild(createButton);
+
+  const getButton = document.createElement("button");
+  getButton.textContent = "get"
+  getButton.addEventListener("click", function() {
+    get({
+      "publicKey": {
+        "timeout": 30000,
+        "challenge": "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=",
+        "allowCredentials": [{
+          "type": "public-key",
+          "id": "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD=="
+        }]
+      }
+    });
+  })
+  document.body.appendChild(getButton);
+})
